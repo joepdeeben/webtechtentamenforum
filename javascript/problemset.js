6af1e747f8f54dc9cc71f918deea6cd6c2e7350d
joepdeeben <jldeeben@gmail.com>
2023-01-10 17:42:28 +0100

    kan nu antwoorden checken

--- javascript/problemset.js
+++ javascript/problemset.js
@@ -6,10 +6,30 @@ var options = [
 
 ];
 
+var answers = [
+                '7',
+                'y = -2',
+                'x² + x',
+                '5'
+
+];
+
+function checkans(){
+          var value = document.getElementById("numb").value;
+          if (value == answers[randOptionNum]) {
+              window.alert("correct!");
+           } else {
+              window.alert("try again");
+           }
+         };
+
+
+
+
 function randprob(){
-         var randOptionNum = Math.floor(Math.random() * options.length);
+         window.randOptionNum = Math.floor(Math.random() * options.length);
          document.getElementById("dothis").innerHTML = '';
          document.getElementById("dothis").innerHTML = options[randOptionNum];
 
 
-}
+};
