7b78ff3c90ab956a01ddfd9d127868a5e652b1d6
joepdeeben <jldeeben@mail.com>
2023-01-11 18:30:43 +0100

    stijl geupdate

--- javascript/problemset.js
+++ javascript/problemset.js
@@ -1,4 +1,4 @@
-var options = [
+var questions = [
                 '2 + 5',
                 '2y + 4 = 0',
                 'x(x+1)',
@@ -18,20 +18,24 @@ function checkans(){
           var value = document.getElementById("numb").value;
           if (value == answers[randOptionNum]) {
               document.getElementById("feedback").innerHTML = "correct!";
-              document.getElementById("feedback").innerHTML.style.color = "green";
            } else {
               document.getElementById("feedback").innerHTML = "try again!";
-              document.getElementById("feedback").innerHTML.style.color = "red";
+              document.getElementById("showanswer").style.visibility = visible;
+
            }
          };
 
+function showans(){
+     document.getElementById("ans").innerHTML = answers[randOptionNum];
 
+
+}
 
 
 function randprob(){
-         window.randOptionNum = Math.floor(Math.random() * options.length);
+         window.randOptionNum = Math.floor(Math.random() * questions.length);
          document.getElementById("dothis").innerHTML = '';
-         document.getElementById("dothis").innerHTML = options[randOptionNum];
+         document.getElementById("dothis").innerHTML = questions[randOptionNum];
 
 
 };
