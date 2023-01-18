a689ef617f521381ee1386e51e91e706d54d6658
joepdeeben <jldeeben@gmail.com>
2023-01-18 16:28:48 +0100

    javascript werkt niet

--- javascript/forum.js
+++ javascript/forum.js
@@ -7,13 +7,14 @@ vakList.forEach(function(vak) {
         }
     }
 
-
-
 function addComment() {
-    let threadPosts = document.getElementById("thread-posts");
-    let newPost = document.createElement("div");
-    newPost.classList.add("post");
-    newPost.innerHTML = "test";
-    threadPosts.appendChild(newPost);
+  let threadPosts = document.getElementById("thread-posts");
+  let textArea = document.querySelector(".content");
+  let postContent = textArea.value;
+  let newPost = document.createElement("div");
+  newPost.classList.add("post");
+  newPost.innerHTML = '<div class="post-info"><div id="username"><b>Username</b></div><div id="datum"><b>Datum</b></div></div><div class="post-content"><h4>Post Titel</h4><p>' + postContent + '</p></div><div class="post-acties"><a href="#">Beantwoorden</a></div>';
+  threadPosts.appendChild(newPost);
+  textArea.value = "";
 }
 
