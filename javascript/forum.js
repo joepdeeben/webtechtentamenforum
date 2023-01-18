591925c251b9bb43b422e5905f565a13fdb04537
joepdeeben <jldeeben@gmail.com>
2023-01-18 16:53:16 +0100

    javascript werkt nu, je kan posten

--- javascript/forum.js
+++ javascript/forum.js
@@ -1,11 +1,11 @@
-let vakList = document.querySelectorAll('.vakken ul li'); // pakt alle vakken in de html van forum
-vakList.forEach(function(vak) {
-    category.addEventListener('click', function() {
-        let vakId = this.id;
-        fetch(vakUrl + vakId) // hier moeten we de link naar de database invoeren en kunnen zo de threads per vak ophalen en weergeven.
-
-        }
-    }
+//let vakList = document.querySelectorAll('.vakken ul li'); // pakt alle vakken in de html van forum
+//vakList.forEach(function(vak) {
+//    category.addEventListener('click', function() {
+//        let vakId = this.id;
+//        fetch(vakUrl + vakId) // hier moeten we de link naar de database invoeren en kunnen zo de threads per vak ophalen en weergeven.
+//
+//        }
+//    }
 
 function addComment() {
   let threadPosts = document.getElementById("thread-posts");
