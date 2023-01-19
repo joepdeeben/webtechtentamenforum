efb4dd05a922580f0ebfa83eea843d022e053b0a
Mark Broerse <mark.broerse@student.uva.nl>
2023-01-19 15:34:21 +0100

    aparte pagina's te zien op de pagina vakken

--- javascript/formopen.js
+++ javascript/formopen.js
@@ -6,3 +6,21 @@ document.getElementById('selectiejaren').style.visibility='collapse'
 function FormJarenClose(){
     document.getElementById('formjaren').style.visibility='collapse';
     }
+
+function ShowYearOne(){
+    document.getElementById('framejaar1').style.visibility='visible';
+    document.getElementById('framejaar3').style.visibility='collapse';
+    document.getElementById('framejaar2').style.visibility='collapse';
+}
+
+function ShowYearTwo(){
+    document.getElementById('framejaar2').style.visibility='visible';
+    document.getElementById('framejaar3').style.visibility='collapse';
+    document.getElementById('framejaar1').style.visibility='collapse';
+}
+
+function ShowYearThree(){
+    document.getElementById('framejaar3').style.visibility='visible';
+    document.getElementById('framejaar1').style.visibility='collapse';
+    document.getElementById('framejaar2').style.visibility='collapse';
+    }
