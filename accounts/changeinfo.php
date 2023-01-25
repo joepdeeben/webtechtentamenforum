17d98bb0ba20c1c2cc77a0c32f57f5f76ebfefb2
olivierstam4 <120393525+olivierstam4@users.noreply.github.com>
2023-01-25 16:25:56 +0100

    accoutns gefixt

--- accounts/changeinfo.php
+++ accounts/changeinfo.php
@@ -15,6 +15,7 @@
 </head>
 
 <body>
+<?php include '../standard/header.php';?>
     <div>
         <div class="page-titel">
           Verander uw gegevens
@@ -56,5 +57,7 @@
        </div>
        
       </div>
+      <?php include '../standard/footer.php';?>
+    
 </body>
 </html>
