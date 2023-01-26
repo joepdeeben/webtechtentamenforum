09ea3e776868a19b207e97ddc7c252c95d42585c
olivierstam4 <120393525+olivierstam4@users.noreply.github.com>
2023-01-26 13:54:34 +0100

    vakkengoed

--- submit/submitex.php
+++ submit/submitex.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <meta name="viewport" content="with=device-width, initialscale=1.0">
     <meta charset="UTF-8">
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;0,600;1,600&display=swap" rel="stylesheet">
     <title>Submit een opgave</title>
@@ -21,17 +21,17 @@
     <form>
     <div class="info-vraag">
         <div class="jaar-vraag">
-            <select name="jaar" id="options">
+            <select name="jaar" id="jaar">
                 <option value="" disabled selected>Kies een jaar</option>
                 <option value="Algemeen">Algemeen</option>
-                <option value="Jaar 1">Jaar 1</option>
-                <option value="Jaar 2">Jaar 2</option>
-                <option value="Jaar 3">Jaar 3</option>
+                <option value="jaar1">Jaar 1</option>
+                <option value="jaar2">Jaar 2</option>
+                <option value="jaar3">Jaar 3</option>
             </select>
         </div>
         <div class="vak-vraag">
-            <select id="choices">
-                <option value="" disabled selected>Kies een vak</option>
+            <select id="vakken" name="vakken">
+            <option value="" disabled selected>Kies een vak</option>
             </select>
         </div>
     </div>
@@ -49,8 +49,28 @@
     <div class="afbeelding">
         <input type="file" accept="image/*">
     </div>
-    </form>
+       </form>
 </div>
+
+
+
+<script>
+var options = {
+  "jaar1": ["Option 1a", "Option 1b", "Option 1c"],
+  "jaar2": ["Option 2a", "Option 2b", "Option 2c"],
+  "jaar3": ["Option 3a", "Option 3b", "Option 3c"]
+};
+
+$("#jaar").change(function() {
+  var selected = $(this).val();
+  var vakken = $("#vakken");
+  vakken.html("");
+  options[selected].forEach(function(opt) {
+    vakken.append($("<option>").text(opt));
+  });
+});
+</script>
+
 <?php include '../standard/footer.php';?>
 
 
