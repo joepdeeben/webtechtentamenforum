83dc88e041a9482c8569c912c79a165dccd912c7
joepdeeben <joep.deeben@student.uva.nl>
2023-01-27 11:52:54 +0100

    geupdatet

--- forum/forumthreads.php
+++ forum/forumthreads.php
@@ -1,21 +1,21 @@
-// <?php
-//     $db = mysqli_connect('hostname','username','password','dbname');
-//
-//     $threadId = mysqli_real_escape_string($db, $_POST['thread-id']);
-//     $sql = "SELECT * FROM posts WHERE thread_id = '$threadId'";
-//     $result = mysqli_query($db, $sql);
-//
-//     $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
-//
-//     if(isset($_POST['submit'])){
-//         $content = mysqli_real_escape_string($db, $_POST['content']);
-//         $username = mysqli_real_escape_string($db, $_POST['username']);
-//         $datum = mysqli_real_escape_string($db, $_POST['datum']);
-//
-//         $sql = "INSERT INTO posts (content, username, datum, thread_id) VALUES ('$content', '$username', '$datum', '$threadId')";
-//         mysqli_query($db, $sql);
-//     }
-// ?>
+<?php
+    $db = mysqli_connect('hostname','username','password','dbname');
+
+    $threadId = mysqli_real_escape_string($db, $_POST['Thread_thread_Id']);
+    $sql = "SELECT * FROM Comment WHERE Thread_thread_Id = '$threadId'";
+    $result = mysqli_query($db, $sql);
+
+    $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    if(isset($_POST['submit'])){
+        $content = mysqli_real_escape_string($db, $_POST['comment_Id']);
+        $username = mysqli_real_escape_string($db, $_POST['User_user_Id']);
+        $datum = mysqli_real_escape_string($db, $_POST['datum']);
+
+        $sql = "INSERT INTO Comment (comment_Id, User_user_Id, datum, Thread_thread_Id) VALUES ('$content', '$username', '$datum', '$threadId')";
+        mysqli_query($db, $sql);
+    }
+?>
 <html>
 <head>
     <link rel="stylesheet" href="../css/stylemain.css">
@@ -29,7 +29,7 @@
                 <h5>Selecteer vak:</h>
                 <select name="vakken">
                 <?php
-                        $sql = "SELECT DISTINCT vakken FROM threads";
+                        $sql = "SELECT DISTINCT threadcourse FROM Thread";
                         $result = mysqli_query($db, $sql);
                         while ($row = mysqli_fetch_array($result)) {
                             echo "<option value='" . $row['vakken'] . "'>" . $row['vakken'] . "</option>";
@@ -38,7 +38,7 @@
                 </select>
                 <ul>
                     <?php
-                        $sql = "SELECT thread_id, thread_name FROM threads WHERE vakken = '$vakken'";
+                        $sql = "SELECT Thread_thread_Id, threadtitle FROM Threads WHERE threadcourse = '$vakken'";
                         $result = mysqli_query($db, $sql);
                         while ($row = mysqli_fetch_array($result)) {
                             echo "<li><a href='?thread_id=" . $row['thread_id'] . "'>" . $row['thread_name'] . "</a></li>";
