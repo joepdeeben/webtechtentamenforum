5016f81bcbf8099bfc773fc5ac85b507b04b38d7
Olivier <olivier.stam@student.uva.nl>
2023-01-27 12:31:23 +0100

    submitq update

--- submit/submitQ.php
+++ submit/submitQ.php
@@ -15,64 +15,7 @@
 <body>
 
 <?php include '../standard/header.php';?>
-
-<div class="subex">
-    <span class="titel-vraag">Stel een Vraag!</span>
-    <form>
-    <div class="info-vraag">
-        <div class="jaar-vraag">
-            <select name="jaar" id="jaar">
-                <option value="" disabled selected>Kies een jaar</option>
-                <option value="Algemeen">Algemeen</option>
-                <option value="jaar1">Jaar 1</option>
-                <option value="jaar2">Jaar 2</option>
-                <option value="jaar3">Jaar 3</option>
-            </select>
-        </div>
-        <div class="vak-vraag">
-            <select id="vakken" name="vakken">
-            <option value="" disabled selected>Kies een vak</option>
-            </select>
-        </div>
-    </div>
-
-    <div class="titel-opgave-sub">
-        <input type="text" id="titelvraag" name="titelvraag" placeholder="Wat is de titel van uw vraag?">
-    </div>
-    <div class="opgave">
-            <textarea id="vraagvolledig" placeholder="Vul hier een geformuleerde vraag in" name="vraagcontent" rows="9" cols="230"></textarea>
-  
-    </div>
-    
-    <div class="afbeelding">
-        Upload een afbeelding bij de opgave: <br>
-        <input type="file" accept="image/*">
-    </div>
-    <div class="submitbtn">
-    <input  type="submit" value="Stel uw vraag!" onclick="return validateForm()">
-</div>
-       </form>
-</div>
-
-
-
-<script>
-var options = {
-  "jaar1": ["Prolog", "Inleiding KI", "Inleiding Logica", "Introductie in programmeren", "Webtechnologie", "Lineare algebra", "Inleiding Cognitieve Psychologie", "Calculus en Optimalisatie", "Taaltheorie en taalverwerking", "Informatievisualisatie", "PAV", "Algemeen jaar 1"],
-  "jaar2": ["Bayesian Statistics for Machine Learning", "Computersystemen", "Computationele logica", "Leren", "Leren en Beslissen", "Datastructuren en Algoritmen", "Natuurlijke taalmodellen en interface", "Cognitive modelling","Introduction to computer vision","Tweedejaarsproject BSc KI", "Oriëntatie op studie en loopbaan", "Algemeen jaar 2"  ],
-  "jaar3": ["Keuzevakken", "Filosofie en AI", "Keuzevak", "Afstudeerproject BSc KI", "Algemeen jaar 3"],
-  "Algemeen": ["Algemeen jaar 1", "Algemeen jaar 2", "Algemeen jaar 3", "Algemeen studie" ]
-};
-
-$("#jaar").change(function() {
-  var selected = $(this).val();
-  var vakken = $("#vakken");
-  vakken.html("");
-  options[selected].forEach(function(opt) {
-    vakken.append($("<option>").text(opt));
-  });
-});
-</script>
+<?php include 'submitqwindow.php';?>
 
 <?php include '../standard/footer.php';?>
 
