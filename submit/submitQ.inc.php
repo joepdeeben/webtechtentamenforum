869f6f0efc0d2b13b220c1726ab4e957491bed1c
Surja Sarker <surja.sarker@student.uva.nl>
2023-01-30 15:22:29 +0100

    submitQ sql php code

---
+++ submit/submitQ.inc.php
@@ -0,0 +1,17 @@
+
+<?php
+$db = mysqli_connect('localhost','joepd','BOSVJpbLRngcsJinhoZzsflhQvneHIbF','kithreads_deb');
+if (!$db) { die("Connection failed: " . mysqli_connect_error()); } echo "Connected successfully";
+
+$vraagjaar = $_POST['jaar'];
+$vraagvak = $_POST['vakken'];
+$vraagtitel = $_POST['titelvraag'];
+$vraagcontent = $_POST['email'];
+$vraagimage = $_POST['w'];
+date_default_timezone_set('Europe/Amsterdam');
+$date = date("d-m-Y");
+
+$sql = "INSERT INTO  (User_user_id, threadtitle, threadcontent, threadschoolyear, threadcourse, threadlikes, dateofpost, image) VALUES ('$logged_in_user_id', '$vraagtitel', '$vraagcontent', '$vraagjaar', '$vraagvak', 0, '$date', '$vraagimage)";
+$result = mysqli_query($db, $sql);
+
+?>
