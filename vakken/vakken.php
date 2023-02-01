2f075fcc73680949bf9952f8db7c6f4c5f051f12
Surja Sarker <surja.sarker@student.uva.nl>
2023-02-01 18:35:03 +0100

    wew

--- vakken/vakken.php
+++ vakken/vakken.php
@@ -15,6 +15,50 @@
 
 </head>
 
+<script>
+function ShowYearOne(){
+    document.getElementById('framejaar1').style.display='block';
+    document.getElementById('framejaar3').style.display='none';
+    document.getElementById('framejaar2').style.display='none';
+    document.getElementById('buttonjaar1').style.background='white';
+    document.getElementById('buttonjaar1').style.color='#304ba3';
+    document.getElementById('buttonjaar2').style.background='#304ba3';
+    document.getElementById('buttonjaar2').style.color='white';
+    document.getElementById('buttonjaar3').style.background='#304ba3';
+    document.getElementById('buttonjaar3').style.color='white'
+}
+
+function ShowYearTwo(){
+    document.getElementById('framejaar2').style.display='block';
+    document.getElementById('framejaar3').style.display='none';
+    document.getElementById('framejaar1').style.display='none';
+    document.getElementById('buttonjaar2').style.background='white';
+    document.getElementById('buttonjaar2').style.color='#304ba3';
+    document.getElementById('buttonjaar1').style.background='#304ba3';
+    document.getElementById('buttonjaar1').style.color='white';
+    document.getElementById('buttonjaar3').style.background='#304ba3';
+    document.getElementById('buttonjaar3').style.color='white';
+}
+
+function ShowYearThree(){
+    document.getElementById('framejaar3').style.display='block';
+    document.getElementById('framejaar1').style.display='none';
+    document.getElementById('framejaar2').style.display='none';
+    document.getElementById('buttonjaar3').style.background='white';
+    document.getElementById('buttonjaar3').style.color='#304ba3';
+    document.getElementById('buttonjaar2').style.background='#304ba3';
+    document.getElementById('buttonjaar2').style.color='white';
+    document.getElementById('buttonjaar1').style.background='#304ba3';
+    document.getElementById('buttonjaar1').style.color='white'
+    }
+
+
+
+
+
+
+</script>
+
 <body>
   <?php include '../standard/header.php'?>
 
