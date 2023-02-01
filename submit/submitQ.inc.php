c520421da9e0f67f615c306210ac0d396d308799
Surja Sarker <surja.sarker@student.uva.nl>
2023-02-01 17:07:23 +0100

    up

--- submit/submitQ.inc.php
+++ submit/submitQ.inc.php
@@ -5,16 +5,18 @@ if (!$db) { die("Connection failed: " . mysqli_connect_error()); } echo "Connect
 
 session_start();
     $user_id = $_SESSION["user_id"];
-
+echo ($user_id);
 $vraagjaar = $_POST['jaar'];
 $vraagvak = $_POST['vakken'];
 $vraagtitel = $_POST['titelvraag'];
 $vraagcontent = $_POST['vraagcontent'];
 $vraagimage = $_POST['vraagimage'];
 date_default_timezone_set('Europe/Amsterdam');
-$date = date("d-m-Y");
+$date = date("Y-m-d");
 
-$sql = "INSERT INTO Thread (User_user_id, threadtitle, threadcontent, threadschoolyear, threadcourse, threadlikes, dateofpost, image) VALUES ('$user_id', '$vraagtitel', '$vraagcontent', '$vraagjaar', '$vraagvak', 0, '$date', '$vraagimage)";
+$sql = "INSERT INTO Thread (User_user_id, threadtitle, threadcontent, threadschoolyear, threadcourse, threadlikes, dateofpost) VALUES ('$user_id', '$vraagtitel', '$vraagcontent', '$vraagjaar', '$vraagvak', 0, '$date')";
 $result = mysqli_query($db, $sql);
+
+header("Location: submitQ.php");
 
 ?>
