c723b61d965915ac4baf95525e9ccabd76bf6b36
Mark Broerse <mark.broerse@student.uva.nl>
2023-02-02 11:26:05 +0100

    cookiebanner bestanden

---
+++ standard/cookiebanner.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <script src="javascript/vakkenfuncties.js"></script>
+    <link rel="stylesheet" href="css/stylemain.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <meta name="viewport" content="width=device-width, initialscale=1.0">
+    <meta charset="UTF-8">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;0,600;1,600&display=swap" rel="stylesheet">
+    <title>cookies</title>
+    <script>
+    
+
+    function cookiesAccept(){
+        var cookies = 'accepted'
+        document.getElementById('cookiepopup').style.display='none';
+    }
+
+    function cookiesDecline(){
+        var cookies='declined';
+        document.getElementById('cookiepopup').style.display='none';
+        setTimeout(cookiePopUp, 10000)
+    }
+
+    function cookiePopUp(){
+        document.getElementById('cookiepopup').style.display='block';
+    }
+    </script>
+</head>
+<body>
+<div id='cookiepopup' class="cookiesbanner" style="display:block">
+Deze website maakt gebruik van cookies. Door gebruik te maken van deze website ga je akkoord met ons cookiebeleid.
+    <button class="cookiebtnaccept" onclick='cookiesAccept();'>Accepteer</button>
+    <button class="cookiebtndecline" onclick='cookiesDecline();'>Weiger</button>
+</div>
+<div><button class='cookiebtnaccept' onclick='cookiePopUp()'>Kom terug</button></div>
+</body>
+</html>
+
