db4d3dcbd7fc49150e8aafc6930c039bb46c84a3
Olivier <olivier.stam@student.uva.nl>
2023-02-03 23:33:41 +0100

    als t goed is alles goed

--- submit/submitex.php
+++ submit/submitex.php
@@ -1,3 +1,12 @@
+
+<?php
+error_reporting(0);
+    session_start();
+    if (!isset($_SESSION['user_id'])) {
+        header("Location: ../accounts/newloginwindow.php");
+      exit;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
