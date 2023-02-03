899e004a310f95c12bf5979173b1aced8880d79a
Surja Sarker <surja.sarker@student.uva.nl>
2023-02-03 16:56:30 +0100

    up

--- submit/submitex.inc.php
+++ submit/submitex.inc.php
@@ -1,7 +1,6 @@
 
 <?php
-$db = mysqli_connect('localhost','joepd','BOSVJpbLRngcsJinhoZzsflhQvneHIbF','kithreads_deb');
-if (!$db) { die("Connection failed: " . mysqli_connect_error()); } echo "Connected successfully";
+include("../includes/dbh.inc.php");
 
 session_start();
     $user_id = $_SESSION["user_id"];
@@ -9,13 +8,25 @@ echo ($user_id);
 $vraagjaar = $_POST['jaar'];
 $vraagvak = $_POST['vakken'];
 $vraagtitel = $_POST['titelvraag'];
+
+$max_length = 250;
 $vraagcontent = $_POST['vraagcontent'];
+$truncated_content = substr($vraagcontent, 0, $max_length);
 $vraaganswer = $_POST['vraaganswer'];
 date_default_timezone_set('Europe/Amsterdam');
 $date = date("Y-m-d");
 
-$sql = "INSERT INTO Exercise (User_user_id, exercisetitle, exercisecontent, exerciseanswer, exercisejaar, exercisecourse, exerciselikes) VALUES ('$user_id', '$vraagtitel', '$vraagcontent', '$vraaganswer', '$vraagjaar', '$vraagvak', 0)";
-$result = mysqli_query($db, $sql);
+$stmt = $db->prepare('INSERT INTO Exercise (User_user_id, exercisetitle, exercisecontent, exerciseanswer, exercisejaar, exercisecourse, exerciselikes) VALUES ( ?, ?, ?, ?, ?, ?, ?)');
+$stmt->bindValue(1, $user_id, PDO::PARAM_INT);
+$stmt->bindValue(2, $vraagtitel, PDO::PARAM_STR);
+$stmt->bindValue(3, $truncated_content, PDO::PARAM_STR);
+$stmt->bindValue(4, $vraaganswer, PDO::PARAM_STR);
+$stmt->bindValue(5, $vraagjaar, PDO::PARAM_STR);
+$stmt->bindValue(6, $vraagvak, PDO::PARAM_STR);
+$stmt->bindValue(7, 0, PDO::PARAM_INT);
+$stmt->execute();
+
+
 
 header("Location: submitex.php");
 
